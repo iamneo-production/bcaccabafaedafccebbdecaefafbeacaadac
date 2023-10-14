f5ac85cd7fe63f6d18b34a264faf6150e298129a
Iamneo <techadmin@iamneo.ai>
2023-10-14 13:01:47 +0000

    Backup Commit

--- demo/src/main/java/com/jdbcassignment/App.java
+++ demo/src/main/java/com/jdbcassignment/App.java
@@ -18,22 +18,35 @@ public class App
         Connection con = null;
         try {
         con = DriverManager.getConnection("jdbc:mysql://localhost/appdb", "root", "examly");
+            if (con )
+
+
+
+
+
+
+
+
+
+
+
+
         //Statement stmt = con.createStatement();
         //String sqlQuery = "Insert into employee values(" + 100 + ", 'emp1', " + 10000 + ");";
         //System.out.println(sqlQuery);
         //stmt.execute(sqlQuery);
-        PreparedStatement stmt = con.prepareStatement("insert into employee values(?,?,?);");
-        stmt.setInt(1, 102);
-        stmt.setString(2, "emp2");
-        stmt.setInt(3, 20000);
-        int count = stmt.executeUpdate();
-        if(count >0)
-        System.out.println("Reconrd inserted successfully");
-        //  if(con !=null) 
-        //     System.out.println("Connection successful");
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        // PreparedStatement stmt = con.prepareStatement("insert into employee values(?,?,?);");
+        // stmt.setInt(1, 102);
+        // stmt.setString(2, "emp2");
+        // stmt.setInt(3, 20000);
+        // int count = stmt.executeUpdate();
+        // if(count >0)
+        // System.out.println("Reconrd inserted successfully");
+        // //  if(con !=null) 
+        // //     System.out.println("Connection successful");
+        // } catch(Exception e) {
+        //     e.printStackTrace();
+        // }
         
     }
 }
