cd3b4d04e9ff8ceba730e95776513bc9179bbe26
Iamneo <techadmin@iamneo.ai>
2023-10-14 05:05:44 +0000

    Backup Commit

---
+++ demo/src/main/java/jdbcFirst.java
@@ -0,0 +1,16 @@
+package demo.src.main.java;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class jdbcFirst {
+    public static void main( String[] args ) throws SQLException
+    {
+        Connection con = null;
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/appdb", "root", "examly");
+        if(con != null) {
+            System.out.println("Connection successful");
+        }
+    }
+}
